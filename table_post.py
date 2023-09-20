bf71f353262618165ea98500d8ec7cd85f283ff5
Dranikf <kobfedsur@gmail.com>
2023-09-20 22:10:52 +0300

    add user.py that describes data model for user data table

--- table_post.py
+++ table_post.py
@@ -1,13 +1,28 @@
 from sqlalchemy import (
     Column,
     Integer,
-    String
+    String,
+    desc
 )
 
-from database import Base
+from database import (
+    Base, 
+    SessionLocal
+)
 
 class Post(Base):
     __tablename__ = "post"
     id = Column(Integer, primary_key = True)
     text = Column(String)
     topic = Column(String)
+
+if __name__ == "__main__":
+    lst = (
+        SessionLocal().
+        query(Post).
+        filter(Post.topic=="business").
+        order_by(desc(Post.id)).
+        limit(10).
+        all()
+    )
+    print([p.id for p in lst])
---
+++ table_user.py
@@ -0,0 +1,18 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    String
+)
+
+from database import Base
+
+class User(Base):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True)
+    gender = Column(Integer)
+    age = Column(Integer)
+    country = Column(String)
+    city = Column(String)
+    exp_group = Column(Integer)
+    os = Column(String)
+    source = Column(String)
