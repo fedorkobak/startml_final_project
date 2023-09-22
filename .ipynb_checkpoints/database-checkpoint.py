065d1f0b6ec4002ecfcb77cd201da307bbb073ae
Fedor Kobak <kobfedsur@gmail.com>
2023-09-22 11:41:44 +0300

    table user add query

---
+++ .ipynb_checkpoints/database-checkpoint.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "postgresql://robot-startml-ro:pheiph0hahj1Vaif@postgres.lab.karpov.courses:6432/startml"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+# psql -h postgres.lab.karpov.courses -p 6432 -U robot-startml-ro startml
