cf1ac80384438367c9fe4136275b7edbe2d90b72
Fedor Kobak <kobfedsur@gmail.com>
2023-11-24 15:45:07 +0300

    data load eda begin

---
+++ lms_loader.py
@@ -0,0 +1,13 @@
+import os
+import pickle
+
+def get_model_path(path: str) -> str:
+    if os.environ.get("IS_LMS") == "1":
+        MODEL_PATH = '/workdir/user_input/model'
+    else:
+        MODEL_PATH = path
+    return MODEL_PATH
+
+def load_models():
+    model_path = get_model_path("/my/super/path")
+    model = pickle.load(model_path)
